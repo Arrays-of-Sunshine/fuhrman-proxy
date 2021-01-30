a9fc1deb1dd9b0da67a61fb49f18496c73eb0e9a
Lexy Kramer <lexy.kramer@gmail.com>
2021-01-29 19:11:34 -0500

    Added all modules, positioned everything, added break lines, added nav bar

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 8000;
 const path = require('path');
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
+app.use('/css', express.static('public/styles.css'));
 
 app.listen(port, () => {
   console.log(`Now listening on port ${port}`);
